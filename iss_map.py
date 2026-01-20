0a47d8610550acfa7f460614c851ba4e7d735bb1
jaydenh-deltion <161732348+JaydenH04@users.noreply.github.com>
2026-01-20 11:23:42 +0100

    map off the location of the iss and my home. there is a line to connect them

--- iss_map.py
+++ iss_map.py
@@ -10,23 +10,35 @@ print(" Creating live map of ISS position... ")
 response = requests.get("http://api.open-notify.org/iss-now.json")
 data = response.json()
 
-latitude = float(data['iss_position']['latitude'])
-longitude = float(data['iss_position']['longitude'])
+latitude = float(data['iss_position']['latitude']) # haal de latitude op
+longitude = float(data['iss_position']['longitude']) # haal de longitude op
 
-iss_map = folium.Map(location=[latitude, longitude], zoom_start=2)
+dedemsvaart = [52.6003, 6.4631]
 
-folium.Marker(
+center_lat = (latitude + dedemsvaart[0]) / 2 # bereken het middenpunt voor de kaart
+center_lon = (longitude + dedemsvaart[1]) / 2 # bereken het middenpunt voor de kaart
+
+iss_map = folium.Map(location=[center_lat, center_lon], zoom_start=3) # maak de kaart aan
+
+folium.Marker( # markeer de ISS positie
     [latitude, longitude],
     popup=f"ISS Position<br>Lat: {latitude}<br> Lon: {longitude}",
     icon=folium.Icon(color='red', icon='rocket', prefix='fa')
 ).add_to(iss_map)
 
-folium.Marker(
+folium.Marker( # markeer Dedemsvaart
     [52.6003, 6.4631],
     popup="Dedemsvaart",
     icon=folium.Icon(color='blue', icon='home', prefix='fa')
 ).add_to(iss_map)
 
-iss_map.save("iss_live_map.html")
+folium.PolyLine( # teken een lijn tussen ISS en Dedemsvaart
+    locations=[[latitude, longitude], dedemsvaart],
+    color='blue',
+    weight=3,
+    opacity=0.8,
+).add_to(iss_map)
+
+iss_map.save("iss_map.html")
 
 print("Map created and saved as iss_map.html")
